0686ac1e39758a8667d179551296b657a0ff5623
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-03-28 22:31:24 +0200

    Last Update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full flex items-center justify-center pb-10">
+      <p className="text-xl md:text-2xl font-normal text-center">
+        Copyright <span className="text-orange-600 font-medium">&copy;</span>
+        2023 Food App. All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
